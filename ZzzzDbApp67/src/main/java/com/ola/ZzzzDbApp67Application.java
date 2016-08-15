c81d69d2667fd479df3c9b3cc455b73634cca8dc
OLAMIDE <OLAMIDE@192.168.2.10>
2016-08-15 15:38:46 -0400

    Initial Commit

---
+++ ZzzzDbApp67/src/main/java/com/ola/ZzzzDbApp67Application.java
@@ -0,0 +1,12 @@
+package com.ola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzzzDbApp67Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzzzDbApp67Application.class, args);
+	}
+}
